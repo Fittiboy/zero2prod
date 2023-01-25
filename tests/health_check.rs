8d8366e5830d66d50e3a349b0e6f071c587565de
Fitti <dev@fitti.io>
2023-01-25 13:38:38 +0100

    Avoid further repitition

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,4 +1,4 @@
-use std::net::{SocketAddr, TcpListener};
+use std::net::TcpListener;
 
 #[tokio::test]
 async fn health_check_works() {
@@ -8,7 +8,7 @@ async fn health_check_works() {
 
     // Act
     let response = client
-        .get(&format!("http://{}/health_check", address.to_string()))
+        .get(&format!("{}/health_check", address))
         .send()
         .await
         .expect("health check request should always be possible to send");
@@ -19,7 +19,7 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-fn spawn_app() -> SocketAddr {
+fn spawn_app() -> String {
     let listener =
         TcpListener::bind("127.0.0.1:0").expect("listener should be able to bind a random port");
     let address = listener
@@ -28,5 +28,5 @@ fn spawn_app() -> SocketAddr {
     let server = zero2prod::run(listener)
         .expect("`run` should be able to bind the random address given by the OS");
     let _ = tokio::spawn(server);
-    address
+    format!("http://{address}")
 }
