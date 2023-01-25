36f06377d689ccea6665f2e4284b4ca49c9f7219
Fitti <dev@fitti.io>
2023-01-25 14:14:11 +0100

    Add comment to `main`

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::net::TcpListener;
 use zero2prod::run;
 
 #[tokio::main]
+// Binds to localhost on port 8000 and runs our server on that address.
 async fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:8000")?;
     run(listener)?.await
