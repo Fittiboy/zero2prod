09cd6d7205e76c81e3b5d8b50a89998e358a1e5d
Fitti <dev@fitti.io>
2023-01-26 15:09:54 +0100

    Add migrations and allow skipping docker setup

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -25,15 +25,18 @@ DB_NAME=${POSTGRES_DB:=newsletter}
 # Check if a custom database port has been set, otherwise default to '5432'
 DB_PORT=${POSTGRES_PORT:=5432}
 
-# Launch postgres using Docker
-docker run \
-    -e POSTGRES_USER=${DB_USER} \
-    -e POSTGRES_PASSWORD=${DB_PASSWORD} \
-    -e POSTGRES_DB=${DB_NAME} \
-    -p "${DB_PORT}":5432 \
-    -d postgres \
-    postgres -N 1000
-    # ^ Increased maximum number of connections for test purposes
+# Launch postgres using Docker, skip if SKIP_DOCKER is set
+if [[ -z "$SKIP_DOCKER" ]]
+then
+    docker run \
+        -e POSTGRES_USER=${DB_USER} \
+        -e POSTGRES_PASSWORD=${DB_PASSWORD} \
+        -e POSTGRES_DB=${DB_NAME} \
+        -p "${DB_PORT}":5432 \
+        -d postgres \
+        postgres -N 1000
+        # ^ Increased maximum number of connections for test purposes
+fi
 
 # Keep pinging Postgres until it's read to accept commands
 export PGPASSWORD="${DB_PASSWORD}"
@@ -42,6 +45,11 @@ until psql -h "localhost" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q' 
     sleep 1
 done
 
+>&2 echo "Postgres is up and running on port ${DB_PORT} - running migrations now!"
+
 DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
 export DATABASE_URL
 sqlx database create
+sqlx migrate run
+
+>&2 echo "Postgres has been migrated, ready to go!"
