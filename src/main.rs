854f0a77ed26b2c7cdae6f4c528817425e11ab8a
Fitti <dev@fitti.io>
2023-01-26 15:44:04 +0100

    Add basic config file and file handling

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use std::net::TcpListener;
+use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
 #[tokio::main]
 // Binds to localhost on port 8000 and runs our server on that address.
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8000")?;
+    let configuration = get_configuration().expect("failed to read configuration");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
     run(listener)?.await
 }
